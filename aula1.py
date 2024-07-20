41b2c782111038ff54afb3e1e66cf280f4799fb8
Adão William Joaquim <adao.joaquim@lojasgregy.com.br>
2024-07-20 01:36:00 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,9 @@
+#comentário
+"""
+DOCSTRING: é quando temos tres aspas  e as usamos para fazer comentários que possam mudar de linhas pois quando 
+fazemos comentários usando a hastag(#), podemos tao somemnte comentar na mesma linha. ja as áspas triplas nos dão
+a chance de escrever comentário em paragfravos. Também podemos usar aspas simples desde que tres vezes('''''')
+"""
+print(123) # no Python, basta inserir a função print para que o codigo seja lido
+print(456),
+
